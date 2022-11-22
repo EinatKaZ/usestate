0ec8319c7fb4ac7eeca9e614a442f0a2f3e699d5
EinatKaZ <einatkazari@gmail.com>
2022-11-22 11:27:46 +0200

    Added useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
         Use state
         <p> Another line</p>
+        <Counter />
     </div>
   );
 }
